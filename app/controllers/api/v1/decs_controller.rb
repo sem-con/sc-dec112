96ed9145a4ca781bcd696ebe65547d834ec96d96
Christoph Fabianek <christoph.fabianek@gmail.com>
2019-12-04 16:23:42 +0100

    API endpoints for Viewer

--- app/controllers/api/v1/decs_controller.rb
+++ app/controllers/api/v1/decs_controller.rb
@@ -7,13 +7,31 @@ module Api
             respond_to :xml, only: []
 
             def index
-                render json: {"hello": "world"}, 
+                content = []
+                Store.pluck(:item).each do |item|
+                    record = JSON(item)
+                    content << { 
+                        "id": record["id"],
+                        "caller_uri": record["caller_uri"],
+                        "duration": Time.at(record["end_ts"].to_datetime.to_i - record["start_ts"].to_datetime.to_i).utc.strftime("%H:%M:%S"),
+                        "message_count": record["messages"].count
+                    }
+                end 
+                render json: content, 
                        status: 200
 
             end
 
             def show
-                render json: {"hello": params[:id].to_s}, 
+                content = []
+                Store.pluck(:item).each do |item|
+                    record = JSON(item)
+                    if record["id"].to_s == params[:id]
+                        content = record
+                    end
+                end                 
+
+                render json: content, 
                        status: 200
 
             end
